50ea63782141d96ab4386f1b80f8d8e174283e79
Szymon Kucharski <speckope@gmail.com>
2021-10-27 20:59:31 +0200

    Added Apollo and graphql-codegen!

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { useHelloQuery } from './generated/graphql';
 
 function App() {
-  return <div className='App'>App</div>;
+  // const { data, loading } = useQuery(gql`
+  //   {
+  //     hello
+  //   }
+  // `);
+
+  const { data, loading } = useHelloQuery();
+
+  if (loading || !data) {
+    return <div>loading...</div>;
+  }
+
+  return <div className='App'>{JSON.stringify(data.hello)}</div>;
 }
 
 export default App;
